9d9d479ebc5fa9576e3b0441907d3c9f502f799b
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 19:01:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,9 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] 
+     programmer_hash[:dennis_ritchie][:languages] do |a,b|
+       puts #{b}.join
+     end 
 end
 
 def adding_matz
