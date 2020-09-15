68c9bc1047f1dcb2e8cf737ff5f087d68a7e8987
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 18:55:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,9 +39,8 @@ def alan_kay_is_known_for
         }
      }
      
-     alan_kay_is_known_for[:alan_kay][:known_for] do |a,b|
-       puts "#{a}"
-     end
+     programmer_hash[:alan_kay][:known_for] 
+    
 end
 
 def dennis_ritchies_language
